5da9cae6b7f627f24312deb80c9408542551751f
SHB910317 <hernandezbarrero@gmail.com>
2021-03-03 01:37:01 -0500

    mvp

--- pruebaGiving/src/app/navbar/navbar.component.ts
+++ pruebaGiving/src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener, Inject } from "@angular/core";
 
+
+declare const window: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  sticky: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+   
   }
+  
 
 }
